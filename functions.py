8fc60b144a159c4aa4c78dc3b893c5f228f4df48
Bill Booth <moronbros@gmail.com>
2016-03-12 14:21:51 -0800

    adding functions.py
    testing functions

---
+++ functions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+"""Some fun work with functions."""
+
+import sys
+
+def is_even(number):
+	if number % 2 == 0:
+		return True
+	else:
+		return False
+
+
+def main():
+	str_number = raw_input('Enter a number: ')
+	their_number = int(str_number)
+	if is_even(their_number):
+		print 'Yes!'
+	else:
+		print 'Nope!'
+
+if __name__ == "__main__":
+	sys.exit(main())
